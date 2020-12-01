17237f65645d89e47211a9946936f625b4f35460
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-12-01 09:17:50 +0530

    Initial application setups and project cleanup

---
+++ src/pages/AppPage/apppage.component.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+/* Styling */
+import "./apppage.styles.scss";
+
+interface Props{
+
+}
+
+const AppPage :React.FC<Props>=()=>{
+	return (
+		<div className="homepage">
+			This is app page
+		</div>
+	)
+
+}
+
+export default AppPage;
---
+++ src/pages/HomePage/homepage.component.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+/* Styling */
+import "./homepage.styles.scss";
+
+interface Props{
+
+}
+
+const HomePage :React.FC<Props>=()=>{
+	return (
+		<div className="homepage">
+			This is homepage
+		</div>
+	)
+
+}
+
+export default HomePage;
